4771e512dece8c7cc3ef564897a2d29620120102
Gontran Bezerra <gontranbezerra@gmail.com>
2021-06-20 13:59:15 -0300

    feat: environments and Pipe Async

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,21 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { CursosService } from './../cursos.service';
 import { Curso } from '../curso';
+import { Observable } from 'rxjs';
 
 @Component({
   templateUrl: './cursos-lista.component.html',
   styleUrls: ['./cursos-lista.component.scss'],
   preserveWhitespaces: true
 })
-export class CursosListaComponent implements OnInit {
+export class CursosListaComponent implements OnInit, OnDestroy {
 
-  cursos!: Curso[];
+  // cursos!: Curso[];
+  cursos$!: Observable<Curso[]>;
 
   constructor(private service: CursosService) { }
 
   ngOnInit(): void {
-    this.service.list().subscribe(dados => this.cursos = dados);
+    //this.service.list().subscribe(dados => this.cursos = dados);
+
+    // this.cursos$ = this.service.list();
+    // this.cursos$.subscribe((dados) => (this.cursos = dados));
+
+    this.cursos$ = this.service.list();
   }
+
+  ngOnDestroy() {
+
+  }
+
+
 
 }
