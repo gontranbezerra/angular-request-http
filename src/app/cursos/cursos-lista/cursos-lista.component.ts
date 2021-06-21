ec96d294b5446a28fc9a1edadd1159c84b2b8b4b
Gontran Bezerra <gontranbezerra@gmail.com>
2021-06-21 07:44:25 -0300

    feat: error catching (async)

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -2,7 +2,8 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { CursosService } from './../cursos.service';
 import { Curso } from '../curso';
-import { Observable } from 'rxjs';
+import { empty, Observable, of, Subject } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   templateUrl: './cursos-lista.component.html',
@@ -13,6 +14,7 @@ export class CursosListaComponent implements OnInit, OnDestroy {
 
   // cursos!: Curso[];
   cursos$!: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
   constructor(private service: CursosService) { }
 
@@ -22,7 +24,40 @@ export class CursosListaComponent implements OnInit, OnDestroy {
     // this.cursos$ = this.service.list();
     // this.cursos$.subscribe((dados) => (this.cursos = dados));
 
-    this.cursos$ = this.service.list();
+    // this.cursos$ = this.service.list();
+
+    this.onRefresh();
+  }
+
+  onRefresh() {
+    this.cursos$ = this.service.list().pipe(
+      catchError((error) => {
+        console.error(error);
+        this.error$.next(true);
+        // return empty();
+        return of<Curso[]>(); // também retorna um Observable vazio como empty().
+      })
+    );
+
+    // Outras possibilidades:
+
+    // this.service.list().subscribe(
+    //   dados => {
+    //     console.log(dados);
+    //   },
+    //   error => console.error(error),
+    //   () => console.log('Observable completo.')
+    // )
+
+    // ou...
+
+    // this.service.list()
+    // .pipe(
+    //   catchError(error => empty()) // catchError() tem que ser o último operador do Pipe()
+    // )
+    // .subscribe(
+    //   (dados) => console.log(dados)
+    // );
   }
 
   ngOnDestroy() {
