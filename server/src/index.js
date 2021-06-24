89aeabf6eb6d70f5195125f6cefb0490fd106de9
Gontran Bezerra <gontranbezerra@gmail.com>
2021-06-23 23:06:20 -0300

    feat: file upload: node.js back-end

---
+++ server/src/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const multiparty = require("connect-multiparty");
+const cors = require("cors");
+
+const app = express();
+
+// parse application/json
+app.use(bodyParser.json())
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+const multipartyMiddleware = multiparty({ uploadDir: './uploads'})
+
+app.post('/upload', multipartyMiddleware, (req, res) => {
+  const files = req.files;
+  console.log('Arquivos recebidos: ', files);
+  res.json({ message: files });
+})
+
+app.use((err, req, res, next) => res.json({ error: err.message }));
+
+// app.use(function (req, res) {
+//   res.setHeader("Content-Type", "text/plain");
+//   res.write("you posted:\n");
+//   res.end(JSON.stringify(req.body, null, 2));
+// });
+
+app.listen(8000, () => {
+  console.log('Servidor rodando na porta 8000...');
+})
+
