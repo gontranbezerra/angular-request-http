c79d39e0083f2c40ad0baa8e65d8e1552c432545
Gontran Bezerra <gontranbezerra@gmail.com>
2021-06-23 23:32:36 -0300

    feat: file upload: FormData request

--- src/app/upload-file/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,17 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UploadFileService } from './../upload-file.service';
+
 @Component({
   selector: 'app-upload-file',
   templateUrl: './upload-file.component.html',
   styleUrls: ['./upload-file.component.scss'],
 })
 export class UploadFileComponent implements OnInit {
-  constructor() {}
+
+  files!: Set<File>;
+
+  constructor(private service: UploadFileService) {}
 
   ngOnInit(): void {}
 
   onChange(event: any) {
     // const selectedFiles = <FileList>event.srcElement.files;
     const selectedFiles = <FileList>event.target.files;
+    this.files = new Set();
+    for (let i = 0; i < selectedFiles.length; i++) {
+      this.files.add(selectedFiles[i]);
+    }
+  }
+
+  onUpload() {
+    if (this.files && this.files.size > 0) {
+      this.service.upload(this.files, 'http://localhost:8000/upload')
+        .subscribe(response => console.log('Upload concluído.'));
+    }
   }
 }
