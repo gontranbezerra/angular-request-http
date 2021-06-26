2914f4725628f2d6f499ec42e5e93ec26419cb33
Gontran Bezerra <gontranbezerra@gmail.com>
2021-06-26 10:15:37 -0300

    feat: setting URL params (HttpParams)

--- src/app/reactive-search/lib-search/lib-search.component.ts
+++ src/app/reactive-search/lib-search/lib-search.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-lib-search',
@@ -10,16 +11,35 @@ import { Observable } from 'rxjs';
 })
 export class LibSearchComponent implements OnInit {
   queryField = new FormControl();
-  // readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
-  // results$!: Observable<any>;
+  readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
+  results$!: Observable<any>;
   total!: number;
-  // readonly FIELDS = 'name,description,version,homepage';
+  readonly FIELDS = 'name,description,version,homepage';
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {}
 
   onSearch() {
-    console.log(this.queryField.value)
+    let search = this.queryField.value;
+    if (search && (search = search.trim()) !== '') {
+
+      // const params = {
+      //   search,
+      //   fields: this.FIELDS,
+      // }
+
+      let params = new HttpParams();
+      params = params.set('search', search);
+      params = params.set('fields', this.FIELDS);
+
+      this.results$ = this.http
+        // .get(`${this.SEARCH_URL}?fields=${this.FIELDS}&search=${search}`)
+        .get(this.SEARCH_URL, { params })
+        .pipe(
+          tap((response: any) => (this.total = response.total)),
+          map((response: any) => response.results)
+        );
+    }
   }
 }
